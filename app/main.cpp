05239e80076d817fc17cf5301a308d4f45baad9c
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-11 01:14:50 +0200

    Refactor: Split to appropriate functions and add documentation

--- app/main.cpp
+++ app/main.cpp
@@ -16,8 +16,34 @@
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-    std::cout << "This is a test to check that everything is working as intended." << std::endl;
 
-    struct knnresult res = knnresult();
-    std::cout << res.ndist << std::endl;
+    // int n = 2;
+    // int m = 3;
+    // int d = 2;
+    // int k = 2;
+    // int X[n][d] = {{3, 3}, {5, 1}};
+    // int Y[m][d] = {{0, 1}, {2, 5}, {4, 1}};
+
+    int n = 7;
+    int m = 3;
+    int d = 1;
+    int k = 3;
+
+    // double **X = new double *[n];
+    // double **Y = new double *[m];
+
+    // for (int i = 0; i < n; i++) {
+    //     X[i] = new double[d];
+    // }
+
+    // for (int i = 0; i < m; i++) {
+    //     Y[i] = new double[d];
+    // }
+
+    double X[][1] = {{1.0}, {2.0}, {4.0}, {15.0}, {16.0}, {18.0}, {40.0}};
+    double Y[][1] = {{0.0}, {17.0}, {30.0}};
+
+    kNN(*X, *Y, n, m, d, k);
+
+    return 0;
 }
--- include/knn.h
+++ include/knn.h
@@ -1,3 +1,12 @@
+#ifndef KNN
+#define KNN
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+
+#define D_MAX std::numeric_limits<double>::max()
+
 /**
  * @brief kNN result struct
  *
@@ -9,16 +18,97 @@ typedef struct knnresult {
     int k;         //!< Number of nearest neighbors            [scalar]
 } knnresult;
 
-//! Compute k nearest neighbors of each point in X [n-by-d]
-/*!
+/**
+ * @brief Add an element to knn arrays and sort ndist[y][:].
+ *
+ * @param dist  Distance between point x (X[j]) and y (Y[i])
+ * @param x     X[j]
+ * @param ndist kNN distances of Y
+ * @param nidx  kNN indeces of Y
+ * @param k     Number of nearest neighbors to search for
+ */
+void addTokNN(double dist, int x, double *ndist, int *nidx, int k) {
 
-  \param  X      Corpus data points              [n-by-d]
-  \param  Y      Query data points               [m-by-d]
-  \param  n      Number of corpus points         [scalar]
-  \param  m      Number of query points          [scalar]
-  \param  d      Number of dimensions            [scalar]
-  \param  k      Number of neighbors             [scalar]
+    // TODO: Binary search for insertion
 
-  \return  The kNN result
-*/
-knnresult kNN(double *X, double *Y, int n, int m, int d, int k);
+    for (int i = k - 1; i > 0; i--) {
+        if (ndist[i - 1] < dist) {
+            for (int j = k - 1; j > i; j--) {
+                ndist[j] = ndist[j - 1];
+                nidx[j]  = nidx[j - 1];
+            }
+            ndist[i] = dist;
+            nidx[i]  = x;
+            break;
+        }
+    }
+    if (ndist[0] >= dist) {
+        for (int j = k - 1; j > 0; j--) {
+            ndist[j] = ndist[j - 1];
+            nidx[j]  = nidx[j - 1];
+        }
+        ndist[0] = dist;
+        nidx[0]  = x;
+    }
+}
+
+/**
+ * @brief Compute k nearest neighbors of each point in X [n-by-d]
+ *
+ * \param  X      Corpus data points              [n-by-d]
+ * \param  Y      Query data points               [m-by-d]
+ * \param  n      Number of corpus points         [scalar]
+ * \param  m      Number of query points          [scalar]
+ * \param  d      Number of dimensions            [scalar]
+ * \param  k      Number of neighbors             [scalar]
+ *
+ * \return  The kNN result
+ */
+knnresult kNN(double *X, double *Y, int n, int m, int d, int k) {
+
+    int **nidx     = new int *[m];
+    double **ndist = new double *[m];
+
+    for (int i = 0; i < m; i++) {
+        nidx[i]  = new int[k];
+        ndist[i] = new double[k];
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < k; j++) {
+            ndist[i][j] = D_MAX;
+        }
+    }
+
+    // TODO: Use BLAS for distance function for n-dimensions.
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            double dist = abs(Y[i * d + 0] - X[j * d + 0]);
+            if (dist < ndist[i][k - 1]) {
+                addTokNN(dist, j, ndist[i], nidx[i], k);
+            }
+        }
+    }
+
+    std::cout << "kNN distances: " << std::endl;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < k; j++)
+            std::cout << ndist[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl << "kNN indeces: " << std::endl;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < k; j++)
+            std::cout << nidx[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+    struct knnresult res = {*nidx, *ndist, m, n};
+
+    return res;
+}
+
+#endif
