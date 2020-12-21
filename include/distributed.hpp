29af1d164efabd701f24dfba62dd6e0c2fd414d0
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-21 20:36:23 +0200

    Gather results to one final struct using Gatherv - Extract functions - Cleanup

--- include/distributed.hpp
+++ include/distributed.hpp
@@ -9,16 +9,6 @@
 
 namespace mpi {
 
-//! Compute distributed all-kNN of points in X
-/*!
-
-  \param  X      Data points                     [n-by-d]
-  \param  n      Number of data points           [scalar]
-  \param  d      Number of dimensions            [scalar]
-  \param  k      Number of neighbors             [scalar]
-
-  \return  The kNN result
-*/
 knnresult distrAllkNN(double* X, int n, int d, int k) {
 
     MPI_Init(NULL, NULL);
@@ -29,34 +19,18 @@ knnresult distrAllkNN(double* X, int n, int d, int k) {
     int process_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
 
-    if (process_rank == 0) {
-        std::cout << "\nX matrix: \n";
-        prt::rowMajor(X, n, d);
-        std::cout << "\n";
-    }
+    // Number of elements to distribute to each process
+    int* chunk_size = new int[world_size];
+    // The displacements where each chunk begins
+    int* displs = new int[world_size];
 
-    int rem         = n % world_size;      // elements remaining after division among processes
-    int sum         = 0;                   // Sum of counts. Used to calculate displacements
-    int* chunk_size = new int[world_size]; // array describing how many elements to send to each process
-    int* displs     = new int[world_size]; // array describing the displacements where each segment begins
-
-    // calculate chunk sizes and displacements
-    for (int i = 0; i < world_size; i++) {
-        chunk_size[i] = (n / world_size) * d;
-        if (rem > 0) {
-            chunk_size[i] += d;
-            rem--;
-        }
-        displs[i] = sum;
-        sum += chunk_size[i];
-    }
+    util::computeChunksDisplacements(chunk_size, displs, world_size, n, d);
 
     // First chunk is the first to be increased in size in case world_size is not perfectly divisible by n
     int MAX_CHUNK_S = chunk_size[0];
-
-    double* _X = new double[chunk_size[process_rank]]; // chunk_size[process_rank] = n/world_size OR n/world_size+1
-    double* _Y = new double[MAX_CHUNK_S];              // set maximum length
-    double* _Z = new double[MAX_CHUNK_S];
+    double* _X      = new double[chunk_size[process_rank]];
+    double* _Y      = new double[MAX_CHUNK_S];
+    double* _Z      = new double[MAX_CHUNK_S];
 
     /* ------------------------------ Distribute X ------------------------------ */
 
@@ -64,32 +38,29 @@ knnresult distrAllkNN(double* X, int n, int d, int k) {
 
     /* ------------------------------ Calculations ------------------------------ */
 
-    memcpy(_Y, _X, MAX_CHUNK_S * sizeof(double));
-    struct knnresult _res = knnresult();
-    _res.m                = chunk_size[process_rank] / d;
-    _res.k                = k;
-    _res.nidx             = new int[_res.m * _res.k]();
-    _res.ndist            = new double[_res.m * _res.k]();
+    knnresult _ans = knnresult();
+    _ans.m         = chunk_size[process_rank] / d;
+    _ans.k         = k;
+    _ans.nidx      = new int[_ans.m * _ans.k]();
+    _ans.ndist     = new double[_ans.m * _ans.k]();
 
-    for (int i = 0; i < _res.m; i++) {
+    for (int i = 0; i < _ans.m; i++) {
         for (int j = 0; j < k; j++) {
-            _res.ndist[i * k + j] = D_MAX;
-            _res.nidx[i * k + j]  = -1;
+            _ans.ndist[i * k + j] = D_MAX;
+            _ans.nidx[i * k + j]  = -1;
         }
     }
 
-    int prev_rank = (world_size + process_rank - 1) % world_size;
-    int next_rank = (world_size + process_rank + 1) % world_size;
+    memcpy(_Y, _X, MAX_CHUNK_S * sizeof(double));
+    kNN(_ans, _Y, _X, displs[process_rank] / d, chunk_size[process_rank] / d, chunk_size[process_rank] / d, d, k);
 
-    kNN(_res, _Y, _X, displs[process_rank] / d, chunk_size[process_rank] / d, chunk_size[process_rank] / d, d, k);
-
-    int cnt = process_rank;
+    int prev_rank              = (world_size + process_rank - 1) % world_size;
+    int next_rank              = (world_size + process_rank + 1) % world_size;
+    unsigned rolling_prev_rank = world_size + process_rank;
 
     for (int i = 0; i < world_size - 1; i++) {
 
-        cnt--;
-        unsigned rolling_prev_rank = util::modulo(cnt, world_size);
-        // std::cout << "Process rank: " << process_rank << " ->\t" << prev_rank << " " << next_rank << std::endl;
+        rolling_prev_rank = --rolling_prev_rank % world_size;
 
         if (process_rank == 0) {
             MPI_Recv(_Z, MAX_CHUNK_S, MPI_DOUBLE, prev_rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -101,31 +72,33 @@ knnresult distrAllkNN(double* X, int n, int d, int k) {
 
         memcpy(_Y, _Z, MAX_CHUNK_S * sizeof(double));
 
-        // std::cout << process_rank << "\t" << displs[util::modulo(cnt, world_size)] / d << " "
-        //           << chunk_size[util::modulo(cnt, world_size)] / d << " " << chunk_size[process_rank] / d << " "
-        //           << _X[0] << std::endl;
-
-        kNN(_res, _Y, _X, displs[rolling_prev_rank] / d, chunk_size[rolling_prev_rank] / d,
+        kNN(_ans, _Y, _X, displs[rolling_prev_rank] / d, chunk_size[rolling_prev_rank] / d,
             chunk_size[process_rank] / d, d, k);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    // std::cout << "Process " << process_rank << " kNN indices:" << std::endl;
-    // prt::rowMajor(_res.ndist, _res.m, _res.k);
-    // // prt::rowMajor(_res.nidx, _res.m, _res.k);
-    // std::cout << std::endl;
+    knnresult ans = knnresult();
+    ans.m         = n;
+    ans.k         = k;
+    ans.nidx      = new int[n * k];
+    ans.ndist     = new double[n * k];
 
-    std::cout << "Process " << process_rank << " kNN distances:" << std::endl;
-    prt::rowMajor(_res.ndist, _res.m, _res.k);
-    std::cout << std::endl;
+    int* recv_chunks = new int[world_size];
+    int* recv_displs = new int[world_size];
+
+    util::computeChunksDisplacements(recv_chunks, recv_displs, world_size, n, k);
+
+    MPI_Gatherv(_ans.nidx, _ans.m * _ans.k, MPI_INT, ans.nidx, recv_chunks, recv_displs, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(_ans.ndist, _ans.m * _ans.k, MPI_DOUBLE, ans.ndist, recv_chunks, recv_displs, MPI_DOUBLE, 0,
+                MPI_COMM_WORLD);
 
     MPI_Finalize();
 
-    // delete[] _X;
-    // delete[] _Y;
-    // delete[] _Z;
-    return _res;
+    if (process_rank == 0)
+        prt::rowMajor(ans.ndist, ans.m, k);
+
+    return ans;
 }
 
 } // namespace mpi
