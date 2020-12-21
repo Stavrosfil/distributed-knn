9f8ca02fa0d783f67f40543893fdcb6557e2d852
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-21 20:37:04 +0200

    Add utilities for chunk, displacements and euclidianDistance calculations

--- include/knn.hpp
+++ include/knn.hpp
@@ -19,82 +19,17 @@ typedef struct knnresult {
     int k;         //!< Number of nearest neighbors            [scalar]
 } knnresult;
 
-void swap(double* a, double* b) {
-    double temp = *a;
-    *a          = *b;
-    *b          = temp;
-}
-
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a       = *b;
-    *b       = temp;
-}
-
-void euclideanDistance(double* X, double* Y, double* D, int n, int m, int d) {
-
-    double* XH = new double[n];
-    double* YH = new double[m];
-
-    // XH = sum(X.^2,2)
-    for (int i = 0; i < n; i++) {
-        XH[i] = 0;
-        for (int j = 0; j < d; j++) {
-            XH[i] += X[i * d + j] * X[i * d + j];
-        }
-    }
-
-    // YH = sum(Y.^2,2)
-    for (int i = 0; i < m; i++) {
-        YH[i] = 0;
-        for (int j = 0; j < d; j++) {
-            YH[i] += Y[i * d + j] * Y[i * d + j];
-        }
-    }
-
-    // D = -2*X*Y.'
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, m, d, -2, X, d, Y, d, 0, D, m);
-
-    // D = sqrt(sum(X.^2,2) - 2*X*Y.' + sum(Y.^2,2).')
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            D[i * m + j] = sqrt(D[i * m + j] + XH[i] + YH[j]);
-        }
-    }
-}
-
 struct less_than_key {
     inline bool operator()(const std::pair<double, int> a, const std::pair<double, int> b) {
         return (a.first < b.first);
     }
 };
 
-/**
- * @brief Compute k nearest neighbors of each point in X [n-by-d]
- *
- * \param  X      Corpus data points              [n-by-d]
- * \param  Y      Query data points               [m-by-d]
- * \param  n      Number of corpus points         [scalar]
- * \param  m      Number of query points          [scalar]
- * \param  d      Number of dimensions            [scalar]
- * \param  k      Number of neighbors             [scalar]
- *
- * \return  The kNN result
- */
 void kNN(knnresult res, double* X, double* Y, int displacement, int n, int m, int d, int k) {
-
-    // std::cout << "X sub-matrix in kNN:\n";
-    // prt::rowMajor(Y, n, d);
-    // std::cout << "\n";
-
-    // std::cout << "================================" << std::endl;
-    // prt::rowMajor(X, d, n);
-    // prt::rowMajor(Y, d, m);
-    // std::cout << n << " " << m << " " << d << " " << std::endl;
 
     double* D = new double[n * m];
 
-    euclideanDistance(X, Y, D, n, m, d);
+    util::computeEuclideanDistance(X, Y, D, n, m, d);
 
     for (int i = 0; i < m; i++) {
 
@@ -111,7 +46,6 @@ void kNN(knnresult res, double* X, double* Y, int displacement, int n, int m, in
         }
 
         std::partial_sort(_D.begin(), _D.begin() + k, _D.end(), less_than_key());
-        // std::nth_element(_D.begin() + (col), _D.begin() + (col + k - 1), _D.begin() + (col + m), less_than_key());
 
         for (int j = 0; j < k; j++) {
             res.ndist[i * k + j] = _D[j].first;
