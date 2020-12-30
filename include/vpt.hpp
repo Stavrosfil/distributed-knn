404724773ca8d01e3033d33504d65adb24f7a131
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-30 23:51:46 +0200

    Working vptree with serialized nodes

--- include/vpt.hpp
+++ include/vpt.hpp
@@ -9,281 +9,74 @@
 class Point {
 
   public:
-    int index;
+    uint32_t index;
+    uint8_t d;
     double* coords;
 
-    Point(int d) {
-        coords = new double[d];
-    }
-
-    Point(double* d) {
-        coords = d;
-    }
-
-    ~Point() {
-        delete coords;
-    }
+    Point(){};
+    Point(int index, double* coords, int d) : index(index), coords(coords), d(d) {}
 };
 
 class Node {
 
   public:
     int vpIndex;
+    int leftIndex;
+    int rightIndex;
+    Point* points;
+
     double mu;
-    Node* left;
-    Node* right;
 
-    Node() : vpIndex(0), mu(0.), left(0), right(0) {}
-
-    ~Node() {
-        delete left;
-        delete right;
-    }
+    Node() : vpIndex(-1), leftIndex(-1), rightIndex(-1), mu(0.), points(0) {}
 };
+
+double distance(Point& p1, Point& p2) {
+    double* D = new double[1];
+    util::computeEuclideanDistance(p1.coords, p2.coords, D, 1, 1, p1.d);
+    return D[0];
+}
 
 class VPT {
 
   public:
-    // double* D                 = {};
-    Node* root;
-    std::vector<double> D     = {};
-    std::vector<Point> points = {};
-    int d                     = 1;
+    std::vector<Point> _points = {};
+    std::vector<Node> _nodes   = {};
 
-    VPT(int d) : d(d) {}
+    VPT(std::vector<Point> points) : _points(points) {}
 
-    void computeDistances() {
-        util::computeEuclideanDistance(data, &vp, D.data(), len, 1, 1);
-    }
-
-    // Partition and construct tree from X[lo:hi]
-    Node* buildTree(int lo, int hi) {
+    // Partition and construct tree from _points[lo:hi]
+    // lo and hi are indices of corpus array
+    int buildTree(int lo, int hi) {
 
         if (lo == hi)
-            return NULL;
+            return -1;
 
-        Node* node    = new Node();
-        node->vpIndex = lo;
+        Node node;
+        node.vpIndex = lo;
 
         if (hi - lo > 1) {
+            int median = (hi + lo) % 2 == 0 ? (hi + lo) / 2 : (hi + lo + 1) / 2;
 
-            int median = (hi + lo) / 2;
-
-            std::nth_element(points.begin() + lo, points.begin() + median, points.begin() + hi + 1,
-                             distanceFromVP(node->vpIndex));
+            node.mu         = computeMu(lo, median, hi);
+            node.leftIndex  = buildTree(lo + 1, median);
+            node.rightIndex = buildTree(median, hi);
         }
+
+        _nodes.push_back(node);
+        return _nodes.size() - 1;
+    }
+
+    double computeMu(int lo, int median, int hi) {
+        std::nth_element(_points.begin() + lo + 1, _points.begin() + median, _points.begin() + hi,
+                         distanceFromVP(_points[lo]));
+        return distance(_points[lo], _points[median]);
     }
 
     struct distanceFromVP {
-        // Point* p;
-        // distanceFromVP(int index) {
-        //     p = new Point(index);
-        // }
-        bool operator()(Point* p1, Point* p2) {
-            return D[p1->index];
+        Point& p;
+        distanceFromVP(Point& p) : p(p) {}
+        bool operator()(Point& p1, Point& p2) {
+            return distance(p, p1) < distance(p, p2);
         }
     };
-
-    // double computeMu() {
-
-    //     double* sortedD = new double[len];
-    //     memcpy(sortedD, D, len * sizeof(double));
-    //     qsort(sortedD, len, sizeof(double), util::compare); // TODO more efficient
-    //     mu = sortedD[(len - 1) / 2];
-    //     // prt::rowMajor(sortedD, 1, len);
-    //     delete[] sortedD;
-    //     return mu;
-    // }
-
-    void createVPT() {
-
-        int len    = -1;
-        int maxIdx = 2 * root.len - 1;
-        // std::cout << "Max Idx = " << maxIdx << std::endl;
-        for (int i = 0; i < maxIdx; i++) {
-            len = tree[i].len;
-            if (len > 1) {
-                tree[i].leftIndex  = tree.size();
-                tree[i].rightIndex = tree.size() + 1;
-                createChildren(tree[i], tree[i].len);
-                // delete[] tree[i].data;
-            }
-        }
-    }
-
-    void createChildren(Node curNode, int len) {
-
-        int leftLen  = (curNode.len + 1) / 2;
-        int rightLen = curNode.len - leftLen;
-        Node _left   = Node(tree.size(), leftLen, computeLeftChild(&curNode), curNode.index);
-        tree.push_back(_left);
-        Node _right = Node(tree.size(), rightLen, computeRightChild(&curNode), curNode.index);
-        tree.push_back(_right);
-    }
-
-    bool isLeaf(int nodeIdx) {
-        if (tree[nodeIdx].leftIndex == -1)
-            return true;
-        else
-            return false;
-    }
-
-    int moveUp(int curNodeIndex) {
-        return tree[curNodeIndex].parentIndex;
-    }
-
-    int moveLeft(int curNodeIndex) {
-        return tree[curNodeIndex].leftIndex;
-    }
-
-    int moveRight(int curNodeIndex) {
-        return tree[curNodeIndex].rightIndex;
-    }
-
-    void vptKnn(knnresult res, double* y, int displacement, int m, int d, int k) {
-
-        // for i = 0 -> i < m
-        int curNodeIndex = tree[0].index;
-        int curNodeLen   = tree[0].len;
-        double* tau      = &res.ndist[k - 1];
-        // std::cout << *tau << std::endl;
-
-        if (checkInside(tree[curNodeIndex].vp, *y, tree[curNodeIndex].mu, *tau)) {
-            searchLeftSubtree(res, curNodeIndex, *y, *tau, displacement, d, k);
-        }
-        if (checkOutside(tree[curNodeIndex].vp, *y, tree[curNodeIndex].mu, *tau)) {
-            searchRightSubtree(res, curNodeIndex, *y, *tau, displacement, d, k);
-        }
-    }
-
-    void searchLeftSubtree(knnresult res, int curNodeIndex, double y, double tau, int displacement, int d, int k) {
-
-        std::cout << "Node " << curNodeIndex
-                  << " inside condition = " << checkInside(tree[curNodeIndex].vp, y, tree[curNodeIndex].mu, tau)
-                  << " -> search left subtree\n";
-        curNodeIndex = moveLeft(curNodeIndex);
-        if (isLeaf(curNodeIndex)) {
-            std::cout << "Reached node " << curNodeIndex << std::endl << std::endl;
-            kNN(res, tree[curNodeIndex].data, &y, displacement, tree[curNodeIndex].len, 1, d, k);
-        } else {
-            if (checkInside(tree[curNodeIndex].vp, y, tree[curNodeIndex].mu, tau)) {
-                searchLeftSubtree(res, curNodeIndex, y, tau, displacement, d, k);
-            }
-            if (checkOutside(tree[curNodeIndex].vp, y, tree[curNodeIndex].mu, tau)) {
-                searchRightSubtree(res, curNodeIndex, y, tau, displacement, d, k);
-            }
-        }
-    }
-
-    void searchRightSubtree(knnresult res, int curNodeIndex, double y, double tau, int displacement, int d, int k) {
-
-        std::cout << "Node " << curNodeIndex
-                  << " outside condition = " << checkOutside(tree[curNodeIndex].vp, y, tree[curNodeIndex].mu, tau)
-                  << " -> search right subtree\n";
-        curNodeIndex = moveRight(curNodeIndex);
-        if (isLeaf(curNodeIndex)) {
-            std::cout << "Reached node " << curNodeIndex << std::endl << std::endl;
-            kNN(res, tree[curNodeIndex].data, &y, displacement, tree[curNodeIndex].len, 1, d, k);
-        } else {
-            if (checkInside(tree[curNodeIndex].vp, y, tree[curNodeIndex].mu, tau)) {
-                searchLeftSubtree(res, curNodeIndex, y, tau, displacement, d, k);
-            }
-            if (checkOutside(tree[curNodeIndex].vp, y, tree[curNodeIndex].mu, tau)) {
-                searchRightSubtree(res, curNodeIndex, y, tau, displacement, d, k);
-            }
-        }
-    }
-
-    bool checkInside(double vp, double y, double mu, double tau) {
-
-        double dist;
-        util::computeEuclideanDistance(&vp, &y, &dist, 1, 1, 1);
-        if (dist < mu + tau)
-            return true;
-
-        return false;
-    }
-
-    bool checkOutside(double vp, double y, double mu, double tau) {
-
-        double dist;
-        util::computeEuclideanDistance(&vp, &y, &dist, 1, 1, 1);
-        if (dist > mu - tau)
-            return true;
-
-        return false;
-    }
-
-    // int searchLeaf(double point) {                      // return the node index of the leaf that contains the point
-
-    //     int curNodeIndex = 0;
-    //     int _leftIdx;
-    //     int _rightIdx;
-    //     while ( !isLeaf(curNodeIndex) ) {
-    //         _leftIdx = tree[curNodeIndex].leftIndex;
-    //         _rightIdx = tree[curNodeIndex].rightIndex;
-    //         if ( belongsToNode(point, tree[_leftIdx]) )
-    //             curNodeIndex = _leftIdx;
-    //         else
-    //             curNodeIndex = _rightIdx;
-    //     }
-    //     return tree[curNodeIndex].index;
-    // }
-
-    // bool belongsToNode(double point, Node curNode) {
-
-    //     for (int i = 0; i < curNode.len; i++) {
-    //         if (curNode.data[i] == point)
-    //             return true;
-    //     }
-    //     return false;
-    // }
-
-    // bool isLeftChild(int curNodeIndex) {
-
-    //     if (curNodeIndex == 0)
-    //         return false;
-    //     if (tree[tree[curNodeIndex].parentIndex].leftIndex == curNodeIndex)
-    //         return true;
-
-    //     return false;
-    // }
-
-    // bool isRightChild(int curNodeIndex) {
-
-    //     if (curNodeIndex == 0)
-    //         return false;
-    //     if (tree[tree[curNodeIndex].parentIndex].rightIndex == curNodeIndex)
-    //         return true;
-
-    //     return false;
-    // }
-}; // END OF VPT CLASS
-
-double* computeLeftChild(Node* parent) {
-
-    int _len      = (parent->len + 1) / 2;
-    double* _data = new double[_len];
-    int idx       = 0;
-    for (int i = 0; i < parent->len; i++) {
-        if (parent->D[i] <= parent->mu)
-            _data[idx++] = parent->data[i];
-    }
-    // std::cout << "Left child:\t";
-    // prt::rowMajor(_data, 1, _len);
-    return _data;
-}
-
-double* computeRightChild(Node* parent) {
-
-    int _len      = parent->len - ((parent->len + 1) / 2);
-    double* _data = new double[_len];
-    int idx       = 0;
-    for (int i = 0; i < parent->len; i++) {
-        if (parent->D[i] > parent->mu)
-            _data[idx++] = parent->data[i];
-    }
-    // std::cout << "Right child:\t";
-    // prt::rowMajor(_data, 1, _len);
-    return _data;
-}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -20,45 +20,40 @@ int main() {
 
     // struct knnresult ans = mpi::distrAllkNN(X, n, d, k);
 
-    double data[]      = {14, 2, 50, 8, 11, 7, 19, 40};
+    // double data[]      = {14, 2, 50, 8, 11, 7, 19, 40};
+    double data[]      = {100, 80, 70, 60, 40, 35, 200, 500};
     int len            = 8;
     int k              = 2;
     int d              = 1;
     double queryPoints = 19;
     int m              = 1;
-    Node root          = Node(0, len, data, -1);
+    // Node root          = Node(0, len, data, -1);
+
+    std::vector<Point> X;
+    for (int i = 0; i < len; i += d) {
+        double* coords = new double[d];
+        for (int j = 0; j < d; j++) {
+            coords[j] = data[i + j];
+        }
+        X.push_back(Point(i, coords, d));
+    }
+
+    VPT vpt(X);
+    std::cout << vpt.buildTree(0, X.size()) << std::endl;
+
+    std::cout << vpt._nodes.size() << std::endl;
+    int cnt = 0;
+    for (auto i : vpt._nodes) {
+        std::cout << cnt++ << ": " << data[i.vpIndex] << "\t"
+                  << "mu: " << i.mu << "\t(" << i.leftIndex << ", " << i.rightIndex << ")" << std::endl;
+        // std::cout << i.vpIndex << std::endl;
+    }
+
+    // for (auto i : X) {
+    //     std::cout << i.coords[0] << std::endl;
+    // }
 
     // std::cout << "mu = " << root.mu << std::endl;
-
-    VPT t = VPT(root);
-    t.createVPT();
-
-    knnresult _ans = knnresult();
-    _ans.m         = m;
-    _ans.k         = k;
-    _ans.nidx      = new int[_ans.m * _ans.k]();
-    _ans.ndist     = new double[_ans.m * _ans.k]();
-
-    for (int i = 0; i < _ans.m; i++) {
-        for (int j = 0; j < k; j++) {
-            _ans.ndist[i * k + j] = D_MAX;
-            _ans.nidx[i * k + j]  = -1;
-        }
-    }
-
-    t.vptKnn(_ans, &queryPoints, 0, _ans.m, d, k);
-
-    std::cout << "\nkNN ndist: ";
-    prt::rowMajor(_ans.ndist, 1, k);
-    std::cout << "\n";
-
-    for (Node n : t.tree) {
-        std::cout << "Node\t" << n.index << ":\t\t";
-        prt::node(n.data, n.len);
-        // std::cout << "parent = " << n.parentIndex << "\tleft = " << n.leftIndex << "\tright = " << n.rightIndex <<
-        // "\tmu = " << n.mu << std::endl;
-    }
-    std::cout << std::endl;
 
     return 0;
 }
